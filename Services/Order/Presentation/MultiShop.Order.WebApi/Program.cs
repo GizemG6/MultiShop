19d461af6f90adb7c5035426dd62d2089b1c34ef
cang8 <0.gizemgunes@gmail.com>
2025-01-23 02:45:54 +0300

    added-OrderingMediatR-on-MultiShop.Order.Application

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,4 +1,10 @@
 
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Persistence.Context;
+using MultiShop.Order.Persistence.Repositories;
+
 namespace MultiShop.Order.WebApi
 {
     public class Program
@@ -8,6 +14,23 @@ namespace MultiShop.Order.WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<OrderContext>();
+
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            #region
+            builder.Services.AddScoped<GetAddressQueryHandler>();
+            builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+            builder.Services.AddScoped<CreateAddressCommandHandler>();
+            builder.Services.AddScoped<UpdateAddressCommandHandler>();
+            builder.Services.AddScoped<RemoveAddressCommandHandler>();
+
+            builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+            builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+            builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+            builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+            builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+            #endregion
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
