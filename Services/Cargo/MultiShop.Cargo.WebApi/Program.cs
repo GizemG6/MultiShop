32f0d281c78f2c72c4f9b69e92ec986fb833304f
cang8 <0.gizemgunes@gmail.com>
2025-01-27 23:12:05 +0300

    edited-IdentityToken-for-MultiShop.Cargo

--- Services/Cargo/MultiShop.Cargo.WebApi/Program.cs
+++ Services/Cargo/MultiShop.Cargo.WebApi/Program.cs
@@ -1,5 +1,10 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.BusinessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
 using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.EntityFramework;
 
 namespace MultiShop.Cargo.WebApi
 {
@@ -8,8 +13,22 @@ namespace MultiShop.Cargo.WebApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = builder.Configuration["IdentityServerUrl"];
+                opt.Audience = "ResourceCargo";
+                opt.RequireHttpsMetadata = false;
+            });
 
             builder.Services.AddDbContext<CargoContext>();
+            builder.Services.AddScoped<ICargoCompanyDal, EfCargoCompanyDal>();
+            builder.Services.AddScoped<ICargoCompanyService, CargoCompanyManager>();
+            builder.Services.AddScoped<ICargoCustomerDal, EfCargoCustomerDal>();
+            builder.Services.AddScoped<ICargoCustomerService, CargoCustomerManager>();
+            builder.Services.AddScoped<ICargoDetailDal, EfCargoDetailDal>();
+            builder.Services.AddScoped<ICargoDetailService, CargoDetailManager>();
+            builder.Services.AddScoped<ICargoOperationDal, EfCargoOperationDal>();
+            builder.Services.AddScoped<ICargoOperationService, CargoOperationManager>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,6 +45,8 @@ namespace MultiShop.Cargo.WebApi
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
