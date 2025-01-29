3339ab88cfc128ad9c5bec0e510e38dd75254ad0
cang8 <0.gizemgunes@gmail.com>
2025-01-29 23:21:15 +0300

    created-MultiShop.DtoLayer

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -30,6 +30,11 @@ namespace MultiShop.WebUI
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            app.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
             app.Run();
         }
     }
