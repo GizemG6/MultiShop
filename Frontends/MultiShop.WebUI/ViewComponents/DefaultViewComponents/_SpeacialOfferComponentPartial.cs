2c4404265142064f7029a62c907d7ce0dd129bd9
cang8 <0.gizemgunes@gmail.com>
2025-01-31 17:30:16 +0300

    completed-DefaultViewComponents-on-MultiShop.WebUI

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpeacialOfferComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.CatalogServices.SpecialOfferServices;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _SpeacialOfferComponentPartial : ViewComponent
+    {
+        private readonly ISpecialOfferService _specialOfferService;
+        public _SpeacialOfferComponentPartial(ISpecialOfferService specialOfferService)
+        {
+            _specialOfferService = specialOfferService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _specialOfferService.GetAllSpecialOfferAsync();
+            return View(values);
+        }
+    }
+}
