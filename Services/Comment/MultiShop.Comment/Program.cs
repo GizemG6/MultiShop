280f2cfdab3e038472ccea1d5394816f03e296c0
cang8 <0.gizemgunes@gmail.com>
2025-02-02 19:39:44 +0300

    added-CommentContext-on-MultiShop.Comment

--- Services/Comment/MultiShop.Comment/Program.cs
+++ Services/Comment/MultiShop.Comment/Program.cs
@@ -1,4 +1,6 @@
 
+using MultiShop.Comment.Context;
+
 namespace MultiShop.Comment
 {
     public class Program
@@ -8,6 +10,7 @@ namespace MultiShop.Comment
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<CommentContext>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
