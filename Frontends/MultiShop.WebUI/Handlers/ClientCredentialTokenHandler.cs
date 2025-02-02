c8689c5c71a28e12b13bf71fe5939d5fe6a03599
cang8 <0.gizemgunes@gmail.com>
2025-02-03 01:14:36 +0300

    added-ClientCredentialTokenHandler-on-MultiShop.WebUI

---
+++ Frontends/MultiShop.WebUI/Handlers/ClientCredentialTokenHandler.cs
@@ -0,0 +1,25 @@
+﻿using MultiShop.WebUI.Services.Interfaces;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace MultiShop.WebUI.Handlers
+{
+    public class ClientCredentialTokenHandler : DelegatingHandler
+    {
+        private readonly IClientCredentialTokenService _clientCredentialTokenService;
+        public ClientCredentialTokenHandler(IClientCredentialTokenService clientCredentialTokenService)
+        {
+            _clientCredentialTokenService = clientCredentialTokenService;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await _clientCredentialTokenService.GetToken());
+            var response = await base.SendAsync(request, cancellationToken);
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                throw new HttpRequestException("Unauthorized request: Access token might be expired or invalid.");
+            }
+            return response;
+        }
+    }
+}
